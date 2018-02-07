7eb173fb8c05316cb263a036bd7ed9ae01bc7218
togayle3651 <togayle3651@L438R4S6.STUDENT.ad.fgcu.edu>
2018-02-06 19:15:52 -0500

    Primitive data types check finished.
    Primitive data types check finished. Next is waiting for certain user
    input.

--- GayleIntegration/src/IntegrationProject.java
+++ GayleIntegration/src/IntegrationProject.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -8,107 +7,101 @@ import java.util.Scanner;
 
 public class IntegrationProject {
 
-  public static void main(String[] args) {
-    Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
 
-    System.out.println("Hello from the integration project!");
+        System.out.println("Hello from the integration project!");
 
-    System.out.print("Loading");
-    slowPrint("...........", 1600);
+        System.out.print("Loading");
+        slowPrint("...........", 1600);
 
-    System.out.println("Please enter your name for login: ");
-    String inputName = "Five Star Notebooks";
+        System.out.println("Please enter your name for login: ");
+        String inputName = "Five Star Notebooks";
 
-    System.out.println("Please enter a number of no particular relevance: ");
-    int inputNum = 5; // input.nextInt();
-    // input.nextLine(); //Move to next line to take inputs normally in the future.
+        System.out.println("Please enter a number of no particular relevance: ");
+        int inputNum = 5; // input.nextInt();
+        // input.nextLine(); //Move to next line to take inputs normally in the future.
 
-    System.out.println("System debugging necessary. Press enter to continue.");
-    input.nextLine();
+        System.out.println("System debugging necessary. Press enter to continue.");
+        input.nextLine();
 
-    { //Print the user's name as an array.
-      String userMessage = "USER: [";
-      String upperName = inputName.toUpperCase().trim();
+        // Print the user in an array format.
+        printStringAsArray(inputName.toUpperCase().trim(), 600L);
 
-      for (int x = 0; x < upperName.length(); x++) { //Loop over each character and print it.
-        char currentLetter = upperName.charAt(x);
+        System.out.println("Beginning core JRE check:");
+        waitTime(500L);
+        System.out.println("Primitive types check:");
+        System.out.println();
 
-        if (x != upperName.length() - 1) {
-          userMessage += (currentLetter + ", ");
-        } else {
-          userMessage += (currentLetter + "]");
+        long primitiveDatatypeCheckWaitLength = 300L;
+        //Print out primitive data types and their ranges.
+        for (PrimitiveTypeModel type: PrimitiveTypeModel.types) {
+            System.out.print(type.getType() + ": exists");
+            slowPrint("..........", primitiveDatatypeCheckWaitLength);
+            System.out.println(
+                    type.getType() + String.format(" range was [%s, %s]%n", type.getMinValue(), type.getMaxValue()));
         }
 
-      }
+        System.out.println("Primitive check complete.");
+        // End primitive datatype check.
 
-      slowPrint(userMessage, 800L);
+        waitTime(500L);
+        System.out.println("Would you like to play a game?");
+        System.out.println("Yes or No?: ");
+        
+        String doesUserWantToPlayAGame = input.nextLine().trim();
+        System.out.println(doesUserWantToPlayAGame + " was in.");
+        System.out.println("doesUserWantToPlayAGame.equalsIgnoreCase(\"y\") was " +doesUserWantToPlayAGame.equalsIgnoreCase("y") );
+        
+        
+        while (!doesUserWantToPlayAGame.equalsIgnoreCase("y")
+                || !doesUserWantToPlayAGame.equalsIgnoreCase("n")) {
+            System.out.println("Please enter a valid input. Yes or No?: ");
+            System.out.println(doesUserWantToPlayAGame + " was in.");
+            System.out.println("doesUserWantToPlayAGame.equalsIgnoreCase(\"y\") was " +doesUserWantToPlayAGame.equalsIgnoreCase("y") );
+            doesUserWantToPlayAGame = input.nextLine().trim();
+        }
+
     }
 
-    System.out.println("Beginning core JRE check:");
-    waitTime(500);
-    System.out.println("Primitive types check:");
-    System.out.println();
+    // Slowly appends the message to the current line over the given time.
+    // Time is given in milliseconds so 1000ms equals 1 second.
+    public static void slowPrint(String message, long time) {
+        for (int i = 0; i < message.length(); i++) {
 
-    long primitiveDatatypeCheckWaitLength = 300L;
+            System.out.print(message.charAt(i));
 
-    System.out.print("byte: exists");
-    slowPrint("..........", primitiveDatatypeCheckWaitLength);
-    System.out.println("byte " + String.format("range was [%d, %d]", Byte.MIN_VALUE, Byte.MAX_VALUE));
-
-    System.out.print("short: exists");
-    slowPrint("..........", primitiveDatatypeCheckWaitLength);
-    System.out.println("short " + String.format("range was [%d, %d]", Short.MIN_VALUE, Short.MAX_VALUE));
-
-    System.out.print("int: exists");
-    slowPrint("..........", primitiveDatatypeCheckWaitLength);
-    System.out.println("int " + String.format("range was [%d, %d]", Integer.MIN_VALUE, Integer.MAX_VALUE));
-
-    System.out.print("long: exists");
-    slowPrint("..........", primitiveDatatypeCheckWaitLength);
-    System.out.println("long " + String.format("range was [%d, %d]", Long.MIN_VALUE, Long.MAX_VALUE));
-
-    System.out.print("float: exists");
-    slowPrint("..........", primitiveDatatypeCheckWaitLength);
-    System.out.println("float " + String.format("range was [%f, %f]", Float.MIN_VALUE, Float.MAX_VALUE));
-
-    System.out.print("double: exists");
-    slowPrint("..........", primitiveDatatypeCheckWaitLength);
-    System.out.println("double " + String.format("range was [%f, %f]", Double.MIN_VALUE, Double.MAX_VALUE));
-
-    System.out.print("boolean: exists");
-    slowPrint("..........", primitiveDatatypeCheckWaitLength);
-    System.out.println("boolean range was [false, true]");
-
-    System.out.print("char: exists");
-    slowPrint("..........", primitiveDatatypeCheckWaitLength);
-    System.out.println("char " + String.format("range was [%s, %s]", Character.MIN_VALUE, Character.MAX_VALUE));
-
-
-
-  }
-
-  //Slowly prints the given message over the given time. Time is given in milliseconds so 1000ms
-  //equals 1 second.
-  static void slowPrint(String message, long time) {
-    for (int i = 0; i < message.length(); i++) {
-
-      System.out.print(message.charAt(i));
-
-      waitTime(time / message.length());
+            waitTime(time / message.length());
+        }
+        System.out.println();
     }
-    System.out.println();
-  }
 
-  static void waitTime(long time) {
-    try {
-      Thread.sleep(time);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
+    public static void waitTime(long time) {
+        try {
+            Thread.sleep(0); //temporarily set to 0 instead of time
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
-  }
 
-  static void checkPrimitives() {
+    /*
+     * Takes a String and prints it over a given time in milliseconds.
+     */
+    public static void printStringAsArray(String str, long time) {
+        String userMessage = "USER: [";
 
-  }
+        for (int x = 0; x < str.length(); x++) { // Loop over each character and print it.
+            char currentLetter = str.charAt(x);
+
+            if (x != str.length() - 1) {
+                userMessage += (currentLetter + ", ");
+            } else {
+                userMessage += (currentLetter + "]");
+            }
+
+        }
+
+        slowPrint(userMessage, time);
+    }
 
 }
